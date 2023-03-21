01f252a6655d642f76a23d9eb2131550f0ec39cd
LaraGEN <laragen@hehecoding.com>
2023-03-21 11:41:46 +0200

    Commit #0 made by LaraGEN @2023-03-21 09:41:46

--- config/app.php
+++ config/app.php
@@ -195,9 +195,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
-    	App\Providers\LaragenServiceProvider::class,
-
-],
+    ],
 
     /*
     |--------------------------------------------------------------------------
