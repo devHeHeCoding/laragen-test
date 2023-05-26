4f60bed2c260e5eb29b805493942797e2f4e2160
LaraGEN <laragen@hehecoding.com>
2023-05-26 11:50:30 +0000

    Commit #0 made by LaraGEN @2023-05-26 11:50:30

--- resources/views/components/layouts/navigation/sidebar-menu.blade.php
+++ resources/views/components/layouts/navigation/sidebar-menu.blade.php
@@ -1,19 +1,66 @@
 <ul class="sidebar__wrapper-menu">
-            <li x-data="{ isOpen: {{in_array(request()->route()->getName(), ['']) ? 'true' : 'false'}} }" :aria-expanded="isOpen">
+    <li>
+        <a href="#" class="sidebar__wrapper-menu-item">
+            <i class="fa-thin fa-house sidebar__wrapper-menu-item-icon"></i>
+            <span class="sidebar__wrapper-menu-item-title">Home</span>
+        </a>
+    </li>
+    <li x-data="{ isOpen: true }" :aria-expanded="isOpen">
         <button type="button" :aria-expanded="isOpen" @click="isOpen = !isOpen"
-                class="sidebar__wrapper-menu-item {{in_array(request()->route()->getName(), ['']) ? 'active' : ''}}">
+                class="sidebar__wrapper-menu-item active">
             <i class="fa-thin fa-pallet-box sidebar__wrapper-menu-item-icon"></i>
-            <span class="sidebar__wrapper-menu-item-title">Mr.</span>
+            <span class="sidebar__wrapper-menu-item-title">Profil</span>
             <i class="fal fa-chevron-down sidebar__wrapper-menu-item-icon-collapse" x-show="!isOpen"></i>
             <i class="fal fa-chevron-up sidebar__wrapper-menu-item-icon-collapse" x-show="isOpen"></i>
         </button>
         <ul :hidden="!isOpen">
-                            <li>
-        <a href="" class="sidebar__wrapper-menu-item {{request()->routeIs('') ? 'active' : ''}}">
-            <i class="Sed quo. sidebar__wrapper-menu-item-icon"></i>
-            <span class="sidebar__wrapper-menu-item-title">Dr.</span>
+            <li>
+                <a href="#" class="sidebar__wrapper-menu-item">
+                    <i class="fa-solid fa-circle sidebar__wrapper-menu-item-icon"></i>
+                    <span class="sidebar__wrapper-menu-item-title">Products</span>
+                </a>
+            </li>
+            <li x-data="{ isOpen: true }" :aria-expanded="isOpen">
+                <button type="button" :aria-expanded="isOpen" @click="isOpen = !isOpen"
+                        class="sidebar__wrapper-menu-item active">
+                    <i class="fa-solid fa-circle sidebar__wrapper-menu-item-icon"></i>
+                    <span class="sidebar__wrapper-menu-item-title">Billing</span>
+                    <i class="fal fa-chevron-down sidebar__wrapper-menu-item-icon-collapse" x-show="!isOpen"></i>
+                    <i class="fal fa-chevron-up sidebar__wrapper-menu-item-icon-collapse" x-show="isOpen"></i>
+                </button>
+                <ul :hidden="!isOpen">
+                    <li>
+                        <a href="#" class="sidebar__wrapper-menu-item active">
+                            <i class="fa-solid fa-circle sidebar__wrapper-menu-item-icon"></i>
+                            <span class="sidebar__wrapper-menu-item-title">Users</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#" class="sidebar__wrapper-menu-item">
+                            <i class="fa-solid fa-circle sidebar__wrapper-menu-item-icon"></i>
+                            <span class="sidebar__wrapper-menu-item-title">Inbox</span>
+                        </a>
+                    </li>
+                </ul>
+            </li>
+            <li>
+                <a href="#" class="sidebar__wrapper-menu-item">
+                    <i class="fa-solid fa-circle sidebar__wrapper-menu-item-icon"></i>
+                    <span class="sidebar__wrapper-menu-item-title">Invoice</span>
+                </a>
+            </li>
+        </ul>
+    </li>
+    <li>
+        <a href="#" class="sidebar__wrapper-menu-item">
+            <i class="fa-thin fa-boxes-packing sidebar__wrapper-menu-item-icon"></i>
+            <span class="sidebar__wrapper-menu-item-title">Materii Prime</span>
         </a>
     </li>
-                    </ul>
+    <li>
+        <a href="#" class="sidebar__wrapper-menu-item">
+            <i class="fa-thin fa-cash-register sidebar__wrapper-menu-item-icon"></i>
+            <span class="sidebar__wrapper-menu-item-title">Achiziții</span>
+        </a>
     </li>
-    </ul>
+</ul>
